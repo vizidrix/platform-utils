50e1eac0096d338f1ebbbdb5c3b15e47436dd022
Perry Birch <perrysd@gmail.com>
2023-01-28 04:45:17 -0800

    Updated worker feature flag

--- crates/route-rs/src/lib.rs
+++ crates/route-rs/src/lib.rs
@@ -6,6 +6,10 @@ mod router;
 pub use lexer::{Lexer, LexerError};
 pub use router::{Router, RouterError};
 
+#[cfg(feature="worker")]
+use worker::{ Env, Request, Result, Response };
+
+#[cfg(feature="worker")]
 pub trait Service {
     fn handler(&mut self, router: &mut Router, req: Request, env: Env, ctx: worker::Context) -> Result<Response>;
 }
