3afb511ced6dba5ac2d4d0f5a21a4e2625b63482
Perry Birch <perrybirch@vizidrix.com>
2024-10-01 17:07:23 +0000

    removed workers dependency and service decls from router

--- crates/route-rs/src/lib.rs
+++ crates/route-rs/src/lib.rs
@@ -5,10 +5,30 @@ pub use lexer::{Lexer, LexerError};
 pub use router::{Router, RouterError};
 pub use std::future::Future;
 
-#[cfg(feature="worker")]
-use worker::{ Env, Request, Result, Response };
+// #[cfg(feature="worker")]
+// use worker::{ Env, Request, Result, Response };
 
-#[cfg(feature="worker")]
-pub trait Service {
-    fn handler(self, router: Router, req: Request, env: Env, ctx: worker::Context) -> impl Future<Output = Result<Response>>;// + Send;
-}
+// #[cfg(feature="worker")]
+// pub trait Service {
+//     fn handler(self, router: Router, req: Request, env: Env, ctx: worker::Context) -> impl Future<Output = Result<Response>>;
+// }
+
+// #[cfg(feature="worker")]
+// pub trait WorkerService {
+//     type Action;
+
+//     fn from_request(self, router: Router, req: Request, env: Env, ctx: worker::Context) -> impl Future<Output = Result<impl Into<Self::Action>>>;
+// }
+
+// pub trait ActionHandler {
+//     type Action;
+//     type ActionResult;
+
+// }
+
+// #[cfg(feature="worker")]
+// pub trait WorkerResponder {
+//     type Response = impl Into<Response>;
+
+//     fn handle(self, action: TAction) -> impl Future<Output = Result<Self::Response>>;
+// }
