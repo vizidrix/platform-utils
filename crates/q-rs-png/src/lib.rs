7262bb6cc96df1d8a250b3c7adb3f39fc0056c60
Perry Birch <perrybirch@vizidrix.com>
2024-10-01 18:00:33 +0000

    added color template to qr image

--- crates/q-rs-png/src/lib.rs
+++ crates/q-rs-png/src/lib.rs
@@ -31,25 +31,48 @@ impl std::fmt::Display for QrPngError {
     }
 }
 
+pub enum ColorTemplate {
+    BlackOnWhite,
+    BlackOnTransparant,
+    WhiteOnTransparant,
+}
+
+impl ColorTemplate {
+    pub fn into_colors(&self) -> (image::LumaA<u8>, image::LumaA<u8>) {
+        match self {
+            ColorTemplate::BlackOnWhite => (image::LumaA([0u8, 255u8]), image::LumaA([255u8, 255u8])),
+            ColorTemplate::BlackOnTransparant => (image::LumaA([0u8, 255u8]), image::LumaA([0u8, 0u8])),
+            ColorTemplate::WhiteOnTransparant => (image::LumaA([255u8, 255u8]), image::LumaA([0u8, 0u8])),
+        }
+    }
+}
+
 pub async fn generate_qr_image(
     data: &str,
+    color_template: ColorTemplate,
 ) -> Result<Vec<u8>, QrPngError> {
     let segments = Segment::make_segments(data);
     let qr = QrCode::encode_segments_advanced(&segments, CodeEcc::Medium,
         Version::new(5), Version::new(5), Some(Mask::new(2)), false).unwrap();
     // let png: ImageBuffer<Luma<u8>, Vec<u8>> = qr.render::<Luma<u8>>().build();
     let size = qr.size;
+    let (on, off) = color_template.into_colors();
     let png = ImageBuffer::from_fn(size as u32, size as u32, |x, y| {
         if qr.get_module(x as i32, y as i32) {
-            image::Luma([0u8])
+            // image::LumaA([0u8, 0u8])
+            // image::Luma([0u8])
+            on
         } else {
-            image::Luma([255u8])
+            off
+            // image::LumaA([255u8, 255u8])
+            // image::Luma([255u8])
         }
     });
     let scale = 8;
     let resized = resize(&png, (size * scale) as u32, (size * scale) as u32, image::imageops::FilterType::Nearest);
     let mut w = Cursor::new(Vec::new());
-    DynamicImage::ImageLuma8(resized)
+    // DynamicImage::ImageLuma8(resized)
+    DynamicImage::ImageLumaA8(resized)
         // .write_to(&mut w, ImageOutputFormat::Png)
         .write_to(&mut w, ImageFormat::Png)?;
     let vec: Vec<_> = w.into_inner();
